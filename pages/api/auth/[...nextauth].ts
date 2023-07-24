83856d68e6d088662876608d5ff1e72cb625854b
Teba_eleven <73474645+Tebaeleven@users.noreply.github.com>
2023-07-24 22:21:41 +0900

    prismaの初期化方法の変更

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,7 +2,7 @@ import NextAuth, { NextAuthOptions } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-import prisma from "../../../lib/prisma";
+import {prisma} from "../../../lib/prisma";
 
 export const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(prisma), //prismアダプターの使用
